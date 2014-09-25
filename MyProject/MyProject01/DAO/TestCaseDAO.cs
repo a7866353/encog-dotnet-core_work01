95caa1d28f1dfb7403afeecd2d4dd3cf0668007d
Wei9504 <Wei@China.com>
2014-09-26 07:58:33 +0800

    增加数据库类(进行中)

--- MyProject/MyProject01/DAO/TestCaseDAO.cs
+++ MyProject/MyProject01/DAO/TestCaseDAO.cs
@@ -1,6 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
-
+using MyProject01.Agent;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +8,35 @@ using System.Text;
 
 namespace MyProject01.DAO
 {
+    class NetworkParamterObject
+    {
+        public string NetworkName { set; get; }
+    }
+
+    class DealLogObject
+    {
+        public int Index { set; get; }
+        public MarketActions Action { set; get; }
+    }
+    class TestEpisodeOjbect
+    {
+        public int DealCount { set; get; }
+
+        public double TotalMark { set; get; }
+
+        public List<DealLogObject> DealLogObjectList { set; get; }
+
+
+    }
     class TestCaseDAO
     {
         public ObjectId _id;
 
+        public NetworkParamterObject NetworkParamter { set; get; }
+
+        public byte[] NetworkData { set; get; }
+
+        public List<TestEpisodeOjbect> TestEpisodeObjectList { set; get; }
 
     }
 }
