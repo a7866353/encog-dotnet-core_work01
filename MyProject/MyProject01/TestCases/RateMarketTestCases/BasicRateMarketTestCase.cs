df6dd3014085d9052cc5eff6ea93d96160e2c95e
linwWork <linwWork@china.com>
2015-04-27 17:30:28 +0800

    * 增加 BasicRateMarketTestCase.cs

---
+++ MyProject/MyProject01/TestCases/RateMarketTestCases/BasicRateMarketTestCase.cs
@@ -0,0 +1,31 @@
+﻿using MyProject01.Controller;
+using MyProject01.Controller.TrainerFactorys;
+using MyProject01.Util.DataObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject01.TestCases.RateMarketTestCases
+{
+    abstract class BasicRateMarketTestCase : BasicTestCase
+    {
+        protected NetworkController controller;
+        protected TrainingData trainData;
+        protected Trainer _train;
+
+        public override void RunTest()
+        {
+            _train.DataList.Add(trainData);
+
+            // start trainning
+            _train.TestName = TestName;
+            _train.Controller = controller;
+            // _train.IterationCount = 0
+            _train.DecisionCtrl = controller.GetDecisionController();
+            _train.RunTestCase();
+
+        }
+    }
+}
---
+++ MyProject/MyProject01/TestCases/RateMarketTestCases/NormalRateMarketTestCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject01.TestCases.RateMarketTestCases
+{
+    class NormalRateMarketTestCase : BasicRateMarketTestCase
+    {
+        public NormalRateMarketTestCase()
+        {
+
+        }
+    }
+}
