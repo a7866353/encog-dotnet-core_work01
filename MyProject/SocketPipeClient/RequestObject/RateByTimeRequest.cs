7ab2d3e0a15d55f990685b1d93d1db6d4d8b68e9
LinHome <linHome>
2016-03-22 22:19:52 +0800

    增加忘记提交的文件

---
+++ MyProject/SocketPipeClient/RequestObject/RateByTimeRequest.cs
@@ -0,0 +1,41 @@
+﻿using SocketTestClient.Sender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocketTestClient.ConnectionContoller;
+
+namespace SocketTestClient.RequestObject
+{
+    class RateByTimeRequest : IRequest
+    {
+        public RequestType OrderType = RequestType.RateDataRequest;
+
+        public string SymbolName;
+        public int TimeFrame;
+        public DateTime StartTime;
+        public DateTime StopTime;
+
+        public byte[] GetBytes()
+        {
+            DataSendBuffer sb = new DataSendBuffer();
+            sb.Add((int)OrderType);
+            sb.Add(SymbolName);
+            sb.Add(TimeFrame);
+            sb.Add(StartTime);
+            sb.Add(StopTime);
+
+            return sb.GetBytes();
+        }
+
+
+        public RateInfo[] RateInfoArray;
+        public void FromBytes(byte[] data, int length)
+        {
+            RateDataIndicateRequest idc = new RateDataIndicateRequest();
+            idc.FromBytes(data, length);
+            RateInfoArray = idc.RateInfoArray;
+        }
+    }
+}
---
+++ MyProject/SocketPipeClient/RequestObject/RateByTimeRequest2.cs
@@ -0,0 +1,37 @@
+﻿using SocketTestClient.Sender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketTestClient.RequestObject
+{
+    class RateByTimeRequest2 : IRequest
+    {
+        public RequestType OrderType = RequestType.RateByTime;
+
+        public string SymbolName;
+        public int TimeFrame;
+        public DateTime StartTime;
+
+        public byte[] GetBytes()
+        {
+            DataSendBuffer sb = new DataSendBuffer();
+            sb.Add((int)OrderType);
+            sb.Add(SymbolName);
+            sb.Add(TimeFrame);
+            sb.Add(StartTime);
+
+            return sb.GetBytes();
+        }
+
+        public RateInfo[] RateInfoArray;
+        public void FromBytes(byte[] data, int length)
+        {
+            RateDataIndicateRequest idc = new RateDataIndicateRequest();
+            idc.FromBytes(data, length);
+            RateInfoArray = idc.RateInfoArray;
+        }
+    }
+}
